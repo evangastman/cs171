0f9db9151e1ee48d55a338012246834f24503d72
David Kaufman <dkaufman@college.harvard.edu>
2015-04-29 23:47:21 -0400

    pie chart now interactive

--- js/mhvis.js
+++ js/mhvis.js
@@ -25,14 +25,13 @@ mhVis = function(_parentElement, _data, _metaData){
  * Method that sets up the SVG and the variables
  */
 mhVis.prototype.initVis = function(){
-
     var that = this;
 
     this.color = d3.scale.category20();
 
-    this.pie = d3.layout.pie()
-    	.sort(null)
-    	.value(function(d) { return d; });
+  this.pie = d3.layout.pie()
+  	.sort(null)
+  	.value(function(d) { return d; });
 
 	this.arc = d3.svg.arc()
 	    .innerRadius(this.radius - 100)
@@ -45,105 +44,64 @@ mhVis.prototype.initVis = function(){
 	  .append("g")
 	    .attr("transform", "translate(" + this.width / 2 + ", 0" + this.height / 2 + ")");
 
-	    this.updateVis();
+    this.updateVis();
 }
-
-
-// /**
-//  * Method to wrangle the data. In this case it takes an options object
-//  * @param _filterFunction - a function that filters data or "null" if none
-//  */
-// mhVis.prototype.wrangleData= function(_filterFunction){
-
-//     // displayData should hold the data which is visualized
-//     this.displayData = this.filterAndAggregate(_filterFunction);
-
-//     //// you might be able to pass some options,
-//     //// if you don't pass options -- set the default options
-//     //// the default is: var options = {filter: function(){return true;} }
-//     //var options = _options || {filter: function(){return true;}};
-// }
-
 
 mhVis.prototype.updateVis = function(){
 	var that = this;
 
-  	this.g = this.svg.selectAll(".arc")
-      .data(this.pie(this.data))
-    .enter().append("g")
-      .attr("class", "arc");
+  	this.path = this.svg.datum(this.data).selectAll("path")
+      .data(this.pie)
+      .enter().append("path")
+        .attr("class", "pie")
+        .attr("fill", function (d,i) {return that.color(i);})
+        .attr("d", this.arc)
+        .each(function (d) {this._current = d;})
 
-  this.g.append("path")
-      .attr("d", this.arc)
-      .style("fill", function(d, i) { return that.color(i); });
-
-  this.g.append("text")
-      .attr("transform", function(d) { return "translate(" + that.arc.centroid(d) + ")"; })
+      console.log(this.arc.centroid(37));
+    this.svg.append("text")
+      .attr("transform", function(d) {
+        console.log(d); 
+        d.outerRadius = that.outerRadius; 
+        d.innerRadius = that.outerRadius;
+        return "translate(" + that.arc.centroid(d[0]) + ")"; 
+      })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
       .text(function(d, i) { return mhcats[i]; });
 }
 
+mhVis.prototype.render = function (data) {
+   var that = this
 
-// /**
-//  * Gets called by event handler and should create new aggregated data
-//  * aggregation is done by the function "aggregate(filter)". Filter has to
-//  * be defined here.
-//  * @param selection
-//  */
-// mhVis.prototype.onSelectionChange= function (selectionStart, selectionEnd){
-    
-//     // if brush is empty, give wrangleData null as its argument
-//     if(selectionStart == "empty") {
-//         this.wrangleData(null)
-//         }
-//     // else wrangle the data to get the data in the brush selection
-//     else {
-//             this.wrangleData(function (d) { 
-//                 return d.time > selectionStart && d.time < selectionEnd
-//             })
-//         }
-
-//     this.updateVis();
-// }
+  this.path = this.svg.datum(this.data).selectAll("path")
+    .data(this.pie)
+  
+   this.path.transition()
+     .duration(750)
+     .attrTween("d", this.arcTween);
+}
 
 
-// /**
-//  * The aggregate function that creates the counts for each age for a given filter.
-//  * @param _filter - A filter can be, e.g.,  a function that is only true for data of a given time range
-//  * @returns {Array|*}
-//  */
-// mhVis.prototype.filterAndAggregate = function(_filter){
+mhVis.prototype.arcTween = function (a) {
+
+var arc = d3.svg.arc()
+      .innerRadius(132.5 - 100)
+      .outerRadius(132.5 - 20);
+
+  var that = this;
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
 
 
-//     // Set filter to a function that accepts all items
-//     // ONLY if the parameter _filter is NOT null use this parameter
-//     var filter = function(){return true;}
-//     if (_filter != null){
-//         filter = _filter;
-//     }
-//     //Dear JS hipster, a more hip variant of this construct would be:
-//     // var filter = _filter || function(){return true;}
+mhVis.prototype.onSelectionChange= function (filteredData){
+    // set data to be the filtered data
+    this.data = filteredData
 
-//     var that = this;
-
-//     // create an array of values for age 0-100
-//     var res = d3.range(100).map(function () {
-//          return 0;
-//     });
-
-//     // accumulate all values that fulfill the filter criterion
-
-//     // TODO: implement the function that filters the data and sums the values
-//     this.data
-//         .filter(filter)
-//         .forEach(function (d) {
-//             d3.range(100).forEach(function (e){
-//                res[e] += d.ages[e]; 
-//             })
-//         })
-
-//     return res;
-
-// }
-
+    // update the bar chart
+    this.render(this.data);
+}
